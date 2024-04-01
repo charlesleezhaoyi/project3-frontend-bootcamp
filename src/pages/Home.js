8b681fe156b4bab987aadc415e08eaf9198db335
qiannnly <qianlinggg@gmail.com>
2024-04-01 22:00:58 +0800

    updated files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,8 @@ import CategoryList from "../components/Dashboard/CategoryList";
 import SearchBar from "../components/Dashboard/SearchBar";
 import useLoadCategories from "../hooks.js/useLoadCategories.js";
 import useLoadBooks from "../hooks.js/useLoadBooks.js";
-import Button from "../components/Common/Button.js";
+import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRounded";
+import ForumRoundedIcon from "@mui/icons-material/ForumRounded";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
@@ -27,8 +28,10 @@ const Home = () => {
   useEffect(() => {
     if (category) {
       const getCategory = async () => {
-        const res = await axios.get(`${BACKEND_URL}/categories/${category}`);
-        const filteredBooks = res.data;
+        const response = await axios.get(
+          `${BACKEND_URL}/categories/${category}`
+        );
+        const filteredBooks = response.data;
         setBookList(filteredBooks);
       };
       getCategory();
@@ -43,11 +46,13 @@ const Home = () => {
       <CategoryList categories={categories} setCategory={setCategory} />
 
       <BookList bookList={bookList} />
-      <div className="flex gap-4 justify-center items-center">
-        <Button
-          label="Post a new book"
-          onClick={() => navigate("/create-newbook")}
-        />
+      <div className="flex gap-4 justify-center items-center my-12">
+        <button onClick={() => navigate("/create-newbook")}>
+          <AddCircleOutlineRoundedIcon fontSize="large" />
+        </button>
+        <button onClick={() => navigate("/forum")}>
+          <ForumRoundedIcon fontSize="large" />
+        </button>
       </div>
     </div>
   );
