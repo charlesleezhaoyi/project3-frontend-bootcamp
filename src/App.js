d4ed27d31508c9bea15ead342fe00718f2e4800b
qiannnly <qianlinggg@gmail.com>
2024-04-14 10:21:36 +0800

    conditionally render app page based on user authentication

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import ErrorPopUp from "./components/Common/ErrorPopUp";
 import NavBar from "./components/Common/NavBar";
 import MenuBookTwoToneIcon from "@mui/icons-material/MenuBookTwoTone";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
   const [errorMessage, setErrorMessage] = useState("");
   const location = useLocation();
   const navigate = useNavigate();
+  const { user, isAuthenticated } = useAuth0();
 
-  useEffect(() => {
-    if (location.pathname === "/") {
-      navigate("/");
-    }
-  }, [location, navigate]);
+  // useEffect(() => {
+  //   if (location.pathname === "/") {
+  //     navigate("/");
+  //   }
+  // }, [location, navigate]);
 
+  console.log(user);
+  console.log(isAuthenticated);
   return (
     <div className="App">
       <NavBar setErrorMessage={setErrorMessage} />
@@ -27,21 +31,23 @@ function App() {
         setErrorMessage={setErrorMessage}
       />
 
-      <div className="hero min-h-screen ">
-        <div className="hero-content flex-col ">
-          <div>
-            <h1 className="text-4xl sm:text-5xl font-bold tracking-wider text-left ml-3 text-center">
-              Book Donation App
-            </h1>
-            <p className="ml-3 py-3 text-sm">
-              A platform to post and to request a book.
-            </p>
+      {!isAuthenticated && (
+        <div className="hero min-h-screen ">
+          <div className="hero-content flex-col ">
             <div>
-              <MenuBookTwoToneIcon fontSize="large" />
+              <h1 className="text-4xl sm:text-5xl font-bold tracking-wider text-left ml-3 text-center">
+                Book Donation App
+              </h1>
+              <p className="ml-3 py-3 text-sm">
+                A platform to post and to request a book.
+              </p>
+              <div>
+                <MenuBookTwoToneIcon fontSize="large" />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
