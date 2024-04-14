24ef6a23808300c91222bbf09a9631296056dc94
ianlauyin <ianlauyin@gmail.com>
2024-04-15 00:32:37 +0800

    Updated First page for redirect logged in user

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import ErrorPopUp from "./components/Common/ErrorPopUp";
 import NavBar from "./components/Common/NavBar";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -9,6 +9,13 @@ import FirstPage from "./pages/FirstPage";
 function App() {
   const [errorMessage, setErrorMessage] = useState("");
   const { isAuthenticated, isLoading } = useAuth0();
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (location.pathname === "/" && isAuthenticated) {
+      navigate("/home");
+    }
+  }, [navigate, location, isAuthenticated]);
 
   return (
     <div className="App">
