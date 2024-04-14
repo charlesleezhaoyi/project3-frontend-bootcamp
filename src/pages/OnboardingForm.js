9bec2c4578a73a13f93f6889f2b70c8e1c0d818c
ianlauyin <ianlauyin@gmail.com>
2024-04-15 02:41:09 +0800

    Updated onboarding button styling

--- src/pages/OnboardingForm.js
+++ src/pages/OnboardingForm.js
@@ -266,14 +266,14 @@ const Onboarding = () => {
         <div className="mt-6 flex items-center justify-end gap-x-6">
           <button
             type="button"
-            className="text-sm font-semibold leading-6 text-gray-900"
+            className="btn btn-ghost"
             onClick={() => logout()}
           >
             Cancel
           </button>
           <button
             type="submit"
-            className="sticky rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="btn btn-accent"
             disabled={sentVerifyEmail}
             onClick={handleVerifyEmailBtnClick}
           >
@@ -281,7 +281,7 @@ const Onboarding = () => {
           </button>
           <button
             type="submit"
-            className="sticky rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="btn btn-success"
             onClick={handleSaveBtnClick}
           >
             Complete
